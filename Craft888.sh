4a89fea3c5a89240abedf3ab2fd2bedb025120b9
Craft888 <Craft888@ukr.net>
2016-04-01 20:47:26 +0300

    master_cleaned

--- Craft888.sh
+++
@@ -1,7 +0,0 @@
-git config --global user.name Craft888
-git config --global user.email Craft888@ukr.net
-git clone https://github.com/Craft888/brainacad.git
-cd brainacad
-git init
-git fetch
-git branch workaround origin/workaround
